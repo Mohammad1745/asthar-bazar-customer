4fcb0c34956d77b5a603ac303d627d4b33dcdb56
Mohammad Ali <mdali2016.227@gmail.com>
2022-03-11 00:54:54 +0600

    [Add] Sign in page content

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="">SignInPage</div>
+  <div class="form m-32 px-24 py-20">
+    <div class="mb-16 text-2xl font-extrabold">Sign In</div>
+    <div class="my-2">
+      <div class="mb-1.5">Phone</div>
+      <input type="text" v-model="phone" class="w-full h-16">
+    </div>
+    <div class="my-2">
+      <div class="mb-1.5">Password</div>
+      <input type="text" v-model="password" class="w-full h-16">
+    </div>
+    <div class="mt-16 ">
+      <button class="bg-green text-sm font-bold w-full h-16">SIGN IN</button>
+    </div>
+    <div class="flex justify-center py-2">Or</div>
+    <div class="">
+      <a class="bg-green-inverted text-sm font-bold flex justify-center items-center w-full h-16" href="">
+        <i class="fab fa-google text-red-400 pr-2"></i> SIGN IN WITH GOOGLE
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SignInPage'
+  name: 'SignInPage',
+  data() {
+    return {
+      phone: '',
+      password: ''
+    }
+  }
 }
 </script>
