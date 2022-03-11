8f38d1300812bf8d4ff1efba84517b1821cbdc12
Mohammad Ali <mdali2016.227@gmail.com>
2022-03-11 14:00:45 +0600

    [Add] product list on home page

---
+++ components/default/home/ProductList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="className">
+    <div class="mx-10 flex flex-wrap  justify-around">
+      <div
+        v-for="i in 10"
+        :key="i"
+        class="product m-5 shadow-xl cursor-pointer">
+        <div class="image">
+          <img src="~assets/images/product_9.jpg" class="w-80 h-80" alt="">
+        </div>
+        <div class="name m-5 flex justify-center text-lg font-bold">
+          বাগদা - বড় সাইজ (হিমায়িত)
+        </div>
+        <div class="price m-5 flex justify-center text-xl font-bold">
+          49.99tk/kg
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  props: ['className'],
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.product {
+  transition: 0.5s;
+}
+.product:hover {
+  transform: scale(1.1);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="">
     <Banner/>
+    <ProductList className="mt-20"/>
   </div>
 </template>
 
 <script>
 import Banner from "../components/default/home/Banner";
+import ProductList from "../components/default/home/ProductList";
 
 export default {
   name: 'IndexPage',
-  components: {Banner},
+  components: {Banner, ProductList},
   data () {
     return {
     }
