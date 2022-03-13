1d98b2207efdeece8e1eb02a5842d2aa62d886fc
Mohammad Ali <mdali2016.227@gmail.com>
2022-03-13 20:09:14 +0600

    [Add] bkash number

--- components/default/home/summary/Profile.vue
+++ components/default/home/summary/Profile.vue
@@ -1,13 +1,16 @@
 <template>
 <div :class="className">
-  <div class="profile ">
+  <div class="profile text-gray-200">
     <div class="header">
       <img src="~assets/images/logo.png" :height="75" :width="75" class="rounded-full" alt="Name" style="border: solid 2px #bbe432;">
     </div>
-    <div class="description text-gray-200 my-10">
+    <div class="description text-sm my-10">
       Donec vitae purus nunc. Morbi faucibus erat sit amet congue mattis. Nullam fringilla faucibus urna, id dapibus erat iaculis ut. Integer ac sem
     </div>
-    <div class="bkash"></div>
+    <div class="bkash">
+      <div>bKash: +880 1888 8888 88</div>
+      <div>bKash: +880 1888 8888 88</div>
+    </div>
   </div>
 </div>
 </template>
