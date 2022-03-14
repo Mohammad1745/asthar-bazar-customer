fdcc87edda7fb9fe019e55b48288f45b6ca5ea91
Mohammad Ali <mdali2016.227@gmail.com>
2022-03-14 12:11:00 +0600

    [Change] dynamic add to cart button

--- components/default/home/ProductList.vue
+++ components/default/home/ProductList.vue
@@ -1,10 +1,11 @@
 <template>
-  <div :class="className">
+  <div :class="className" class="bg-white z-2">
     <div class="mx-10 flex flex-wrap  justify-around">
       <div
         v-for="i in 10"
         :key="i"
         class="product m-5 shadow-xl cursor-pointer">
+        <div v-if="i%3===0" class="new">NEW</div>
         <div class="image">
           <img src="~assets/images/product_9.jpg" class="w-80 h-80" alt="">
         </div>
@@ -14,6 +15,11 @@
         <div class="price m-5 flex justify-center text-xl font-bold">
           49.99tk/kg
         </div>
+        <div v-if="user" class="flex justify-center mb-5">
+          <NuxtLink class="shop-btn bg-green-inverted text-sm font-bold px-5 py-4" to="/shop">
+            Add To Cart
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </div>
@@ -22,7 +28,7 @@
 <script>
 export default {
   name: "ProductList",
-  props: ['className'],
+  props: ['className', 'user'],
   data(){
     return {
 
@@ -37,5 +43,16 @@ export default {
 }
 .product:hover {
   transform: scale(1.1);
+}
+.new {
+  position: absolute;
+  margin-left: 16rem;
+  background: #bbe432;
+  color: #414141;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4rem;
+  height: 2.5rem;
 }
 </style>
--- pages/shop.vue
+++ pages/shop.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="">
-    <ProductList className="mt-20"/>
+    <Header/>
+    <ProductList user="true" className="pt-20"/>
     <Summary className="mt-20"/>
   </div>
 </template>
@@ -9,6 +10,7 @@
 import Banner from "../components/default/home/Banner";
 import ProductList from "../components/default/home/ProductList";
 import Summary from "../components/default/home/Summary";
+import Header from "../components/user/shop/Header";
 
 export default {
   name: 'ShopPage',
@@ -16,7 +18,7 @@ export default {
   head:{
     title: "Home - Asthar Bazar - Promises 100% Reliance"
   },
-  components: { Banner, ProductList, Summary },
+  components: { Header, ProductList, Summary },
   data () {
     return {
     }
